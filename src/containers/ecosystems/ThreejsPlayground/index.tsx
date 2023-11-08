5f59284dab034c6e5aba74a1d50acf7d6d89f995
NIRANKEN <nirank.8192cpjpyjs@gmail.com>
2023-11-08 17:21:27 +0900

    wip: reactでの生存期間のある動的なオブジェクトお試し

--- src/containers/ecosystems/ThreejsPlayground/index.tsx
+++ src/containers/ecosystems/ThreejsPlayground/index.tsx
@@ -1,55 +1,114 @@
 import { Box } from '@mui/material';
-import { Canvas, MeshProps, useFrame } from '@react-three/fiber';
-import { useRef, useState } from 'react';
-import { Mesh, BufferGeometry, Material, Vector3 } from 'three';
+import { Canvas, MeshProps, Object3DNode, useFrame } from '@react-three/fiber';
+import React, { Suspense, useRef, useState } from 'react';
+import {
+  Mesh,
+  BufferGeometry,
+  Material,
+  Vector3,
+  Camera,
+  PerspectiveCamera,
+  OrthographicCamera,
+} from 'three';
 import { Model } from './Dog002';
 import { PointerLockControls, Sky } from '@react-three/drei';
-import { Physics, RigidBody } from '@react-three/rapier';
+import { Physics, PhysicsProps, RigidBody } from '@react-three/rapier';
 import { Player } from './Player';
 import { Ground } from './Ground';
+import { Grenade } from './Grenade';
+
+type GrenadeType = {
+  id: string;
+  idx: number;
+  onExploded: () => void;
+  // position: [x: number, y: number, z: number];
+};
 
 export const ThreejsPlayground: React.FC = () => {
   const shadowOffset = 50;
+  // const cameraRef = useRef<CameraRefType>();
+  // const [globalGeometry, setGlobalGeometry] =
+  //   useState<BufferGeometry<NormalBufferAttributes>>();
+  // const [globalMaterial, setGlobalMaterial] = useState<Material>();
+  // const globalGeometry = new SphereGeometry(1, 32, 32);
+  // const globalMaterial = new MeshStandardMaterial({ color: 'hotpink', metalness: 0.8, roughness: 0.1 });
+  // const [displayGrenade, setDisplayGrenade] = useState<boolean>(false);
+  const [grenades, setGrenades] = useState<GrenadeType[]>([]);
+  // const grenades: React.FC[] = [];
+  // const grenades: GrenadeType[] = [];
+  const [grenadeIndex, setGrenadeIndex] = useState<number>(0);
+
   return (
-    <Box component="div" width={'100%'} height={1024}>
-      <Box
-        component="div"
-        sx={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          width: '10px',
-          height: '10px',
-          borderRadius: '50%',
-          // transform: 'translate3d(-50%, 50%, 0)',
-          border: '2px solid white',
-          zIndex: 2,
-        }}
-      />
-      <Canvas camera={{ fov: 45, position: [0, 0, 0] }} shadows>
-        <PointerLockControls />
-        <Sky sunPosition={[100, 10, 100]} />
-        <directionalLight
-          castShadow
-          position={[0, 20, 0]}
-          intensity={5}
-          shadow-mapSze={4096}
-          shadow-camera-top={shadowOffset}
-          shadow-camera-bottom={-shadowOffset}
-          shadow-camera-left={shadowOffset}
-          shadow-camera-right={-shadowOffset}
+    <Box component="div" width={'auto'} height={'80vh'}>
+      <Suspense fallback={null}>
+        <Box
+          component="div"
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            width: '10px',
+            height: '10px',
+            borderRadius: '50%',
+            // transform: 'translate3d(-50%, 50%, 0)',
+            border: '2px solid white',
+            zIndex: 2,
+          }}
         />
-        <Physics gravity={[0, -9.8, 0]}>
-          <Ground />
-          <Player />
-          <ThreeBox boxMeshProps={{ position: [0, 0.5, 0] }} />
-          <ModelWrapper position={[10, 0, -20]} />
-          <ModelWrapper position={[5, 0, -20]} />
-          <ModelWrapper position={[0, 0, -20]} />
-          <ModelWrapper position={[-5, 0, -20]} />
-          <ModelWrapper position={[-10, 0, -20]} />
-        </Physics>
-      </Canvas>
+        <Canvas
+          camera={{ fov: 45, position: [0, 0, 0] }}
+          shadows
+          onClick={() => {
+            const idx = grenadeIndex + 1;
+            setGrenadeIndex(idx);
+            setGrenades([
+              ...grenades,
+              {
+                id: `grenade-${idx}`,
+                idx: idx,
+                onExploded: () => {
+                  console.log('onExploded');
+                  setGrenades(
+                    grenades.filter((grenade) => grenade.idx !== idx)
+                  );
+                },
+              },
+            ]);
+          }}
+        >
+          <PointerLockControls />
+          <Sky sunPosition={[100, 10, 100]} />
+          <directionalLight
+            castShadow
+            position={[0, 20, 0]}
+            intensity={5}
+            shadow-mapSze={4096}
+            shadow-camera-top={shadowOffset}
+            shadow-camera-bottom={-shadowOffset}
+            shadow-camera-left={shadowOffset}
+            shadow-camera-right={-shadowOffset}
+          />
+          <Physics gravity={[0, -9.8, 0]}>
+            <Ground />
+            <Player />
+            {/* <Grenade
+            globalGeometry={globalGeometry}
+            globalMaterial={globalMaterial}
+          /> */}
+            {grenades.map((grenade) => (
+              // <Grenade key={grenade.id} position={grenade.position} />
+              <Grenade onExploded={grenade.onExploded} />
+            ))}
+            {/* <Grenade position={grenadePosition} /> */}
+            <ThreeBox boxMeshProps={{ position: [0, 0.5, 0] }} />
+            <ModelWrapper position={[10, 0, -20]} />
+            <ModelWrapper position={[5, 0, -20]} />
+            <ModelWrapper position={[0, 0, -20]} />
+            <ModelWrapper position={[-5, 0, -20]} />
+            <ModelWrapper position={[-10, 0, -20]} />
+          </Physics>
+        </Canvas>
+      </Suspense>
     </Box>
   );
 };
